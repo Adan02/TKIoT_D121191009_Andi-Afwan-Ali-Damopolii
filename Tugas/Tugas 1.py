a721383be05cd74a5e6fe0d18c7a38e6a89c48a8
Andi Afwan Ali Damopolii <70624824+Adan02@users.noreply.github.com>
2022-10-13 22:20:47 +0800

    Fixing kesalahan variabel

--- Tugas/Tugas 1.py
+++ Tugas/Tugas 1.py
@@ -5,8 +5,9 @@ PIRIn= Pin(5,Pin.IN)
 Buzzer = Pin(13,Pin.OUT)
 
 while True:
+    if PIRIn.value()==1:
+        Buzzer.on()
+    if PIRIn.value()==0:
+        Buzzer.off()
     print(p.value())
-    if p.value()==1:
-        o.on()
-    if p.value()==0:
-        o.off()
+    time.sleep(1)
